2f022b175a30c489d00e49d832d9f61c21bf2a03
aditi-1400 <aditisingh1400@gmail.com>
2020-10-05 17:31:05 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop:$($(this).attr('href')).offset().top
+        }, 2000);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop:0
+        }, 2000);
+    });
+    
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once:true
+    });
+}); 
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+
