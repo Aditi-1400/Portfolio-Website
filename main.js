85078a047cef413828a34430eb2f77827bf34492
aditi-1400 <aditisingh1400@gmail.com>
2020-11-04 18:27:40 +0530

    Improving UI

--- main.js
+++ main.js
@@ -49,3 +49,9 @@ anime.timeline({loop: true})
 
 
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
